610c3d2e124e358ee90ea5d930c85852c7bd18a5
Henrique Pacheco Alves <henriquepachecoalves7@gmail.com>
2026-02-26 20:07:59 -0300

    security commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+
 export default function RootLayout() {
-  return <></>;
+  return (
+    <View style={styles.container}>
+      <View></View>
+      <Text>Oi tudo bem?</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
