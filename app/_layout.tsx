de9f2ba1a5e003c906c5f1df790173ddb0a17ee2
Henrique Pacheco Alves <henriquepachecoalves7@gmail.com>
2026-03-04 20:31:46 -0300

    create component button

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,10 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function RootLayout() {
   const [loading, setLoading] = useState();
 
-  return (
-    <View style={styles.fatherContainer}>
-      <View></View>
-      <Text style={styles.input}>Oi tudo bem?</Text>
-    </View>
-  );
+  return <View></View>;
 }
 
 const styles = StyleSheet.create({
