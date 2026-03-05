e3bd3255a6e2c100771f09f13b43b601e9859917
Henrique Pacheco Alves <henriquepachecoalves7@gmail.com>
2026-03-04 22:16:50 -0300

    start create button component

--- components/button.tsx
+++ components/button.tsx
@@ -1,5 +1,46 @@
-const Button = () => {
-  return <></>;
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+interface Props {
+  variant: "numbers" | "operations" | "functions";
+  onPress?: () => void;
+  title: string;
+}
+
+const Button: React.FC<Props> = ({ variant, onPress, title }) => {
+  const getButtonStyle = () => {
+    switch (variant) {
+      case "operations":
+        return styles.operationsButton;
+      case "numbers":
+        return styles.numbersButton;
+      case "functions":
+        return styles.functionsButton;
+    }
+  };
+
+  return (
+    <TouchableOpacity style={getButtonStyle()}>
+      <Text style={styles.textStyle}>{title}</Text>
+    </TouchableOpacity>
+  );
 };
+
+const styles = StyleSheet.create({
+  operationsButton: {
+    backgroundColor: "#256AE0",
+  },
+  numbersButton: {
+    backgroundColor: "#444C5C",
+  },
+  functionsButton: {
+    backgroundColor: "#505B6C",
+  },
+  textStyle: {
+    color: "#FFFF",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
 
 export default Button;
